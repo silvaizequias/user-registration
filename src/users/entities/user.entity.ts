ea62a7dc5ca88aea338645d6f0488717053273b9
Izequias Silva <master@dedicado.digital>
2024-06-27 17:57:00 -0300

    feature: initial user entity model implemented

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,17 @@
-export class User {}
+export class User {
+  id: string
+  createdAt: Date
+  updatedAt: Date
+  deletedAt: Date
+  softDeleted: boolean
+  name: string
+  email: string
+  emailVerified: Date
+  passHash: string
+  zipCode: string
+  street: string
+  complement: string
+  district: string
+  city: string
+  state: string
+}
