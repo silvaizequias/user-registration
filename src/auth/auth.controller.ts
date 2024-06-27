b17ea6cbae9eb2349ddb63f128f2aa896ca7af33
Izequias Silva <master@dedicado.digital>
2024-06-27 18:30:35 -0300

    update: initial auth structure updated

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Post } from '@nestjs/common'
 import { AuthService } from './auth.service'
+import { LoginAuthDto } from './dto/login-auth.dto'
+import { ResetPasswordAuthDto } from './dto/reset-password-auth.dto'
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post()
+  login(@Body() loginAuthDto: LoginAuthDto) {
+    return this.authService.login(loginAuthDto)
+  }
+
+  @Post()
+  resetPassword(@Body() resetPasswordAuthDto: ResetPasswordAuthDto) {
+    return this.authService.resetPassword(resetPasswordAuthDto)
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common'
+import { LoginAuthDto } from './dto/login-auth.dto'
+import { ResetPasswordAuthDto } from './dto/reset-password-auth.dto'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async login(loginAuthDto: LoginAuthDto) {
+    return loginAuthDto
+  }
+
+  async resetPassword(resetPasswordAuthDto: ResetPasswordAuthDto) {
+    return resetPasswordAuthDto
+  }
+}
