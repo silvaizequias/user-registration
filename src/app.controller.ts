5714e6b98f42f16a0b202e5276bbd479d5889d34
Izequias Silva <master@dedicado.digital>
2024-06-27 19:34:33 -0300

    feature: page routes initial implemented

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,22 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello()
+  homePage(): string {
+    return 'Home Page'
+  }
+
+  @Get('login')
+  loginPage(): string {
+    return 'Login Page'
+  }
+
+  @Get('/signup')
+  signup(): string {
+    return 'Register Page'
+  }
+
+  @Get('reset-password')
+  resetPassword(): string {
+    return 'Reset Password Page'
   }
 }
