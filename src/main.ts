501d0ec74bc3220055488142f017fe74f4ccab5a
Izequias Silva <master@dedicado.digital>
2024-06-27 18:42:51 -0300

    feature: main application default structure setted

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,41 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { ValidationPipe } from '@nestjs/common'
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+async function main() {
+  const PORT = process.env.PORT ?? ''
+
+  const app = await NestFactory.create(AppModule, { rawBody: true })
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  )
+
+  app.enableCors({
+    origin: [`http://localhost:${PORT}`],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    credentials: true,
+  })
+
+  const options = new DocumentBuilder()
+    .setTitle('User Registration')
+    .setDescription('User registration application.')
+    .setVersion('1.0')
+    .addBearerAuth({
+      type: 'http',
+      in: 'header',
+      description: 'Authorization',
+    })
+    .build()
+
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('swagger', app, document)
+
+  await app
+    .listen(PORT || 3000)
+    .then(() => console.log(`server running on http://localhost:${PORT}`))
 }
-bootstrap()
+main()
